cca5188237e6e20b0ec2635687e90189669065b1
hugocortesm <hugo.cortes@stkmx37067.softtek.com>
2013-04-04 11:28:03 -0600

    Commit string property

--- src/main/java/com/pech/webdashboard/reporttest/vo/Element.java
+++ src/main/java/com/pech/webdashboard/reporttest/vo/Element.java
@@ -4,45 +4,54 @@ import java.io.Serializable;
 
 public class Element implements Serializable {
 
-	private static final long serialVersionUID = 5377085676577463471L;
+    private static final long serialVersionUID = 5377085676577463471L;
 
 	private Integer id;
 	private String elementId;
 	private String name;
 	private String description;
 	private Integer oneNumber;
+	private String newProperty;
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getElementId() {
-		return elementId;
-	}
+    public String getElementId() {
+        return elementId;
+    }
 
-	public void setElementId(String elementId) {
-		this.elementId = elementId;
-	}
+    public void setElementId(String elementId) {
+        this.elementId = elementId;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getNewProperty() {
+        return newProperty;
+    }
+
+    public void setNewProperty(String newProperty) {
+        this.newProperty = newProperty;
+    }
 
 	public Integer getOneNumber() {
 		return oneNumber;
