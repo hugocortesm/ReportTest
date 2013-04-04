21558422e06eca1e2d92501ee4a273b7db63afdf
hugocortesm <hugo.cortes@stkmx37067.softtek.com>
2013-04-04 11:35:25 -0600

    big decimal property

--- src/main/java/com/pech/webdashboard/reporttest/vo/Element.java
+++ src/main/java/com/pech/webdashboard/reporttest/vo/Element.java
@@ -1,17 +1,19 @@
 package com.pech.webdashboard.reporttest.vo;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 public class Element implements Serializable {
 
     private static final long serialVersionUID = 5377085676577463471L;
 
-	private Integer id;
-	private String elementId;
-	private String name;
-	private String description;
-	private Integer oneNumber;
-	private String newProperty;
+    private Integer id;
+    private String elementId;
+    private String name;
+    private String description;
+    private Integer oneNumber;
+    private String newProperty;
+    private BigDecimal bigProperty;
 
     public Integer getId() {
         return id;
@@ -53,11 +55,20 @@ public class Element implements Serializable {
         this.newProperty = newProperty;
     }
 
-	public Integer getOneNumber() {
-		return oneNumber;
-	}
+    public Integer getOneNumber() {
+        return oneNumber;
+    }
 
-	public void setOneNumber(Integer oneNumber) {
-		this.oneNumber = oneNumber;
-	}
+    public void setOneNumber(Integer oneNumber) {
+        this.oneNumber = oneNumber;
+    }
+
+    public BigDecimal getBigProperty() {
+        return bigProperty;
+    }
+
+    public void setBigProperty(BigDecimal bigProperty) {
+        this.bigProperty = bigProperty;
+    }
+
 }
